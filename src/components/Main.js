b41d9817a7ca8cf01af382e4eb8c5a0fe4cf7d3b
Stephen Wisner <spwisner@gmail.com>
2017-07-12 19:53:57 -0400

    Nested Route for AuctionView Successful

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import AuctionIndex from './auction/AuctionIndex';
 import Home from './Home';
-import {Switch, Route} from "react-router-dom"
+import {Switch, Route} from "react-router-dom";
+import AuctionView from './auction/AuctionView';
+
+const Auction = () => (
+  <Switch>
+    <Route exact path='/auction' component={AuctionIndex} />
+    <Route path='/auction/:id' component={AuctionView} />
+  </Switch>
+)
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Home}/>
-      <Route path='/auction' component={AuctionIndex}/>
+      <Route path='/auction' component={Auction}/>
     </Switch>
   </main>
 );
