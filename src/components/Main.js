9c1b281f1ad589e816ff0d449b9c218614d0576f
Stephen Wisner <spwisner@gmail.com>
2017-07-16 18:13:55 -0400

    Auction GET Request Successful
    - For console.log

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,22 +4,71 @@ import Home from './Home';
 import {Switch, Route} from "react-router-dom";
 import AuctionView from './auction/AuctionView';
 import SignIn from './navigation/prelogin/SignIn';
+import CredentialsStore from '../stores/CredentialsStore';
+import Auction from './auction/Auction';
 
-const Auction = () => (
-  <Switch>
-    <Route exact path='/auction' component={AuctionIndex} />
-    <Route path='/auction/:id' component={AuctionView} />
-  </Switch>
-)
 
-const Main = () => (
-  <main>
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/sign-in' component={SignIn}/>
-      <Route path='/auction' component={Auction}/>
-    </Switch>
-  </main>
-);
 
-export default Main;
+// componentDidMount: function() {
+//     CommonStore.addChangeListener(this._handleChangeStore);
+// },
+//
+// componentDidMount() {
+//   CredentialsStore.on("change", () => {
+//     this.setState({
+//       isSignedIn: CredentialsStore._getUserStatus(),
+//     });
+//   });
+// }
+//
+// const Auction = () => (
+//   <Switch>
+//     <Route exact path='/auction' component={AuctionIndex} />
+//     <Route path='/auction/:id' component={AuctionView} />
+//   </Switch>
+// )
+
+export default class Main extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isSignedIn: CredentialsStore._getUserStatus()
+    }
+  }
+
+  componentDidMount() {
+    CredentialsStore.on("change", () => {
+      this.setState({
+        isSignedIn: CredentialsStore._getUserStatus(),
+      });
+    });
+  }
+
+  render() {
+    const isSignedIn = this.state.isSignedIn;
+    return (
+      <main>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          {isSignedIn ?
+          <Route path='/auction' component={Auction}/>
+          : null  }
+        </Switch>
+      </main>
+    )
+  }
+}
+
+// const Main = () => (
+//   <main>
+//     <Switch>
+//       <Route exact path='/' component={Home}/>
+//       {isSignedIn ?
+//       <Route path='/auction' component={Auction}/>
+//       : null  }
+//     </Switch>
+//   </main>
+// );
+//
+// export default Main;
--- src/components/auction/AuctionIndex.js
+++ src/components/auction/AuctionIndex.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import CredentialsStore from '../../stores/CredentialsStore';
+import {withRouter} from "react-router-dom";
+const apiAuctions = require('../../api/api-auctions');
 
-export default class AuctionIndex extends React.Component {
+class AuctionIndex extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isSignedIn: CredentialsStore._getUserStatus()
+    }
+  }
+
+  componentDidMount() {
+    CredentialsStore.on("change", () => {
+      this.setState({
+        isSignedIn: CredentialsStore._getUserStatus()
+      })
+    });
+    apiAuctions.getAuctions()
+      .done((response) => {
+        console.log(response);
+
+        return;
+      })
+      .fail((response) => {
+        if (response.statusText === "Unauthorized") {
+          return console.log('fail: Unauthorized');
+        } else if (response.statusText === "error") {
+          return console.log('server error');
+        }
+      });
+
+  }
+
+  signInApproved(state) {
+    if (!state) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   render() {
+    const userSignedIn = this.signInApproved(this.state.isSignedIn);
+    console.log(userSignedIn);
     return (
       <div>
-        <p>Auction Index Component</p>
+        {userSignedIn ?
+        <h3 className="text-underline">Auction Items</h3>
+
+        : null
+        }
       </div>
     )
   }
 }
+
+export default withRouter(AuctionIndex);
