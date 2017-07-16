490a25722661c0c7580d94c5cf1595d6dd427d3d
Stephen Wisner <spwisner@gmail.com>
2017-07-16 18:13:24 -0400

    Added AJAX requests for Auction

--- src/api/api-auctions.js
+++ src/api/api-auctions.js
@@ -1,65 +1,72 @@
-// const store = require('../store');
-// const apiOrigin = store.apiOrigin;
-// const userToken = store.user.token;
-//
-// // Auctions API
-//
-// const getAuctions = function() {
-//   return $.ajax({
-//     url: `${apiOrigin}/api/auctions`,
-//     method: 'GET',
-//     headers: {
-//       Authorization: `Token token=${userToken}`,
-//     },
-//   });
-// };
-//
-// const showAuction = function(id) {
-//   return $.ajax({
-//     url: `${apiOrigin}/api/auctions/${id}`,
-//     method: 'GET',
-//     headers: {
-//       Authorization: `Token token=${userToken}`,
-//     },
-//   });
-// };
-//
-// const createAuction = function(data) {
-//   return $.ajax({
-//     url: `${apiOrigin}/api/auctions`,
-//     method: 'POST',
-//     headers: {
-//       Authorization: `Token token=${userToken}`,
-//     },
-//     data,
-//   });
-// };
-//
-// const deleteAuction = function(id) {
-//   return $.ajax({
-//     url: `${apiOrigin}/api/auctions/${id}`,
-//     method: 'DELETE',
-//     headers: {
-//       Authorization: `Token token=${userToken}`,
-//     },
-//   });
-// };
-//
-// const updateAuction = function(data, id) {
-//   return $.ajax({
-//     url: `${apiOrigin}/api/auctions/${id}`,
-//     method: 'PATCH',
-//     headers: {
-//       Authorization: `Token token=${userToken}`,
-//     },
-//     data,
-//   });
-// };
-//
-// module.exports = {
-//   getAuctions,
-//   createAuction,
-//   deleteAuction,
-//   showAuction,
-//   updateAuction,
-// };
+
+import CredentialsStore from '../stores/CredentialsStore';
+const store = require('../store');
+const $ = require('jquery');
+
+// Auctions API
+
+const getAuctions = function() {
+  const userToken = CredentialsStore._getUserToken();
+  console.log(userToken);
+  return $.ajax({
+    url: `${store.apiOrigin}/api/auctions`,
+    method: 'GET',
+    headers: {
+      Authorization: `Token token=${userToken}`,
+    },
+  });
+};
+
+const showAuction = function(id) {
+  const userToken = CredentialsStore._getUserToken();
+  return $.ajax({
+    url: `${store.apiOrigin}/api/auctions/${id}`,
+    method: 'GET',
+    headers: {
+      Authorization: `Token token=${userToken}`,
+    },
+  });
+};
+
+const createAuction = function(data) {
+  const userToken = CredentialsStore._getUserToken();
+  return $.ajax({
+    url: `${store.apiOrigin}/api/auctions`,
+    method: 'POST',
+    headers: {
+      Authorization: `Token token=${userToken}`,
+    },
+    data,
+  });
+};
+
+const deleteAuction = function(id) {
+  const userToken = CredentialsStore._getUserToken();
+  return $.ajax({
+    url: `${store.apiOrigin}/api/auctions/${id}`,
+    method: 'DELETE',
+    headers: {
+      Authorization: `Token token=${userToken}`,
+    },
+  });
+};
+
+const updateAuction = function(data, id) {
+  const userToken = CredentialsStore._getUserToken();
+  return $.ajax({
+    url: `${store.apiOrigin}/api/auctions/${id}`,
+    method: 'PATCH',
+    headers: {
+      Authorization: `Token token=${userToken}`,
+    },
+    data,
+  });
+};
+
+module.exports = {
+  getAuctions,
+  createAuction,
+  deleteAuction,
+  showAuction,
+  updateAuction,
+};
