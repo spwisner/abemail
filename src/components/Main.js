8bf49f3affc930f9900bc9ebe629d580f8e3705b
Stephen Wisner <spwisner@gmail.com>
2017-07-18 14:04:58 -0400

    Updated path in order to incorporate new Component
    - New "smart" component being CampaignContainer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Home from './Home';
 import {Switch, Route} from "react-router-dom";
+// import CredentialsStore from '../stores/CredentialsStore';
+import Campaign from './campaign/Campaign';
 
 function Main() {
   return (
     <main>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route path='/campaigns' component={Campaign}/>
       </Switch>
     </main>
   )
